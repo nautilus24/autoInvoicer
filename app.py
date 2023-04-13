d7bd5bbbb34eed12ab3dc511cf839d10574c3e36
Nautilus <nautilus@pop-os.localdomain>
2023-04-13 14:59:34 -0400

    Second Commit For Cleaner Code

--- app.py
+++ app.py
@@ -1,18 +1,3 @@
-# from flask import Flask,render_template
-
-# app = Flask(__name__)
-
-# @app.route('/login')
-# def login():
-# 	return render_template('login.html')
-
-# @app.route('/home')
-# def home():
-#     return render_template('home.html')
-
-
-# if __name__ == '__main__':
-# 	app.run()
 from flask import Flask, render_template
 
 app = Flask(__name__)
