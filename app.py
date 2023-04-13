f37a3920134b07ef6531830961325f479f45eac6
Nautilus <nautilus@pop-os.localdomain>
2023-04-13 14:56:07 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,37 @@
+# from flask import Flask,render_template
+
+# app = Flask(__name__)
+
+# @app.route('/login')
+# def login():
+# 	return render_template('login.html')
+
+# @app.route('/home')
+# def home():
+#     return render_template('home.html')
+
+
+# if __name__ == '__main__':
+# 	app.run()
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['DEBUG'] = True  # Set debug mode to True
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return "This is the dashboard"
+@app.route('/place_order')
+def place_order():
+    return "This is the dashboard"
+
+if __name__ == '__main__':
+    app.run()
